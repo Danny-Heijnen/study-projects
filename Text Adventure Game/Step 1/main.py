8bd95f3269d3de3153ad7c58fcc7f4e5a507ec30
Danny-Heijnen <78494232+Danny-Heijnen@users.noreply.github.com>
2021-02-04 11:22:33 +0100

    Final version of Step 1

---
+++ Text Adventure Game/Step 1/main.py
@@ -0,0 +1,7 @@
+print("You wake up early in the morning in a forest, next to a burnt-out bonfire.\nA stone's throw away you can see a road.\n")
+direction = input("Do you want to go north (1) or south (2) on that road? ")
+
+if direction == "1":
+    print("You have gone north. After a few miles you find a stone with a strange green glow. You pick up the stone and sink into a deep sleep.")
+else:
+    print("You have gone south. Past a bend in the road, you find an abandoned hut. You decide to make the hut your home and live happily ever after.")

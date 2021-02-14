2777a63f2e53bdaec623fcbbbc54e8219a486abe
Danny-Heijnen <78494232+Danny-Heijnen@users.noreply.github.com>
2021-02-14 17:29:03 +0100

    Create custom password generator

---
+++ Password Generator/Custom Password/Main.py
@@ -0,0 +1,35 @@
+# TODO define function to return the strength of the password
+def return_strength(number_of_chars, capital, number, special_char):
+
+
+
+# Check if the input is a number.
+def input_is_number():
+    try:
+        int(string)
+        return True
+    except ValueError:
+        return False
+
+
+# Request numerical input from the user.
+def getInput(input_string):
+    value = input(input_string)
+    while not value.isnumeric():
+        print("enter a number")
+        value = input("enter again")
+    return int(value)
+
+
+def main():
+
+# TODO work out how you want to handle requesting numerical input
+
+
+
+
+
+
+
+
+main()

af900d5507ff1845cae92b9697697bf9ac301970
Danny-Heijnen <danny_heijnen@outlook.com>
2021-02-27 21:51:21 +0100

    First working version of Word Cloud

--- Word Cloud/README.md
+++ Word Cloud/README.md
@@ -1,2 +1,5 @@
 Basic setup for the program was made with code from:
 https://www.askpython.com/python/examples/word-cloud-using-python
+
+tophat.png downloaded from:
+https://www.pngarts.com/explore/156230
--- Word Cloud/main.py
+++ Word Cloud/main.py
@@ -1,13 +1,36 @@
 import wikipedia
 from wordcloud import WordCloud, STOPWORDS
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
 
 
 def get_wiki(query):
     title = wikipedia.search(query)[0]
+    print(title)
 
     # get wikipedia page for selected title
     page = wikipedia.page(title)
     return page.content
 
 
-print(get_wiki('python programming language'))
+def create_wordcloud(text):
+
+    mask = np.array(Image.open('tophat.png'))
+
+    stopwords = set(STOPWORDS)
+
+    # create wordcloud object
+    wc = WordCloud(background_color='white', max_words=200, mask=mask, stopwords=stopwords)
+
+    wc.generate(text)
+
+    # save wordcloud
+    wc.to_file('output.png')
+
+
+# print(get_wiki('python programming language'))
+
+topic = input('Please enter the topic for the word cloud: ')
+create_wordcloud(get_wiki(topic))

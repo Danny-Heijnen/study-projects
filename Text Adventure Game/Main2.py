973e57110607c54d09b605b9b7c80d0c292252c8
Danny-Heijnen <danny_heijnen@outlook.com>
2021-03-05 11:43:00 +0100

    Major update to Text Adventure Game

--- Text Adventure Game/Main2.py
+++ Text Adventure Game/Main2.py
@@ -13,12 +13,13 @@ import random
 class Room:
 
     # Initialize a Room object to create a Room with 4 texts to use in the encounter.
-    def __init__(self, room_name_text, description_text, attack_text, flee_text, health_potion_text):
+    def __init__(self, room_name_text, description_text, attack_success_text, attack_fail_text, flee_success_text, flee_fail_text):
         self.room_name = room_name_text
         self.description = description_text
-        self.attack = attack_text
-        self.flee = flee_text
-        self.health_potion = health_potion_text
+        self.attack_success = attack_success_text
+        self.attack_fail = attack_fail_text
+        self.flee_success = flee_success_text
+        self.flee_fail = flee_fail_text
 
     def get_text_room_name(self):
         return self.room_name
@@ -26,23 +27,27 @@ class Room:
     def get_text_description(self):
         return self.description
 
-    def get_text_attack(self):
-        return self.attack
+    def get_text_attack_success(self):
+        return self.attack_success
 
-    def get_text_flee(self):
-        return self.flee
+    def get_text_attack_fail(self):
+        return self.attack_fail
 
-    def get_text_health_potion(self):
-        return self.health_potion
+    def get_text_flee_success(self):
+        return self.flee_success
+
+    def get_text_flee_fail(self):
+        return self.flee_fail
 
 
 # Describe a class Player that can be used in the different encounters.
 class Player:
 
+    # TODO: set self.potion back to 0 for the real game.
     # Initialize a Player object to create a player with 2 health and 0 health potions. This player will be used to play the game.
     def __init__(self):
         self.health = 2
-        self.potion = 1
+        self.potion = 0
 
     # Return the current health of the player.
     def get_player_health(self):
@@ -56,8 +61,8 @@ class Player:
             return False
 
     # Take an amount of damage away from the current hit points of the player.
-    def deal_damage(self, damage):
-        self.health = self.health - damage
+    def deal_damage(self):
+        self.health -= 1
 
     # Decrease the number of potions by 1, and increase the health of the player by 1.
     def use_potion(self):
@@ -82,6 +87,14 @@ class Player:
 
 
 # TODO:
+def find_potion(player):
+
+    player.add_potion()
+
+    print("In the room you also find the famous health potion that you have been looking for.\nThe red liquid is so bright, it seems to glow.\nYou notice small glimmers of light or reflecions, it is hard to tell, but the sight is mesmerizing.\nYou carefully place the health potion in your bag.\n")
+
+
+# TODO:
 def use_potion(player):
 
     player.use_potion()
@@ -89,6 +102,28 @@ def use_potion(player):
     print("\nYou pull out the health potion and look at it.\nYou realize that this is the reason you came to this dungeon in the first place.\nBut you also know that you need it to survive.\n\nYou drink the life-giving red fluid and feel your life force return.")
 
     print("\nYour remaining hit points are now: " + str(player.get_player_health()))
+
+
+def attack_action(player, room):
+
+    chance = random.randrange(4)
+
+    if chance < 2:
+        print("\n" + room.get_text_attack_success())
+    else:
+        player.deal_damage()
+        print("\n" + room.get_text_attack_fail())
+
+
+def flee_action(player, room):
+
+    chance = random.randrange(4)
+
+    if chance < 3:
+        print("\n" + room.get_text_attack_success())
+    else:
+        player.deal_damage()
+        print("\n" + room.get_text_attack_fail())
 
 
 # Create a room_playlist with a number of room objects.
@@ -141,7 +176,7 @@ def create_room_playlist():
 
 
 # Render an encounter where the player enters a room and makes a choice.
-def encounter(player, room):
+def encounter(player, room, room_index, final_room_index):
 
     # Clear the terminal screen.
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -158,6 +193,10 @@ def encounter(player, room):
     # Print the description of the current room or situation.
     print("\n" + room.get_text_description() + "\n")
 
+    # Give the player a health potion at the start of room 2.
+    if room_index == 1:
+        find_potion(player)
+
     # Describe the choice to attack or flee.
     print("You can perform the following actions:\n(1) Attack the creature\n(2) Flee from the creature")
 
@@ -169,8 +208,15 @@ def encounter(player, room):
     action = "a"
 
     # Request an action from the player.
-    while not input_validation2(action):
-        action = int(input("\nWhat action do you want to take? "))
+    if player.has_potion():
+        while not input_validation2(action):
+            action = input("\nWhat action do you want to take? ")
+
+    if not player.has_potion():
+        while not input_validation3(action):
+            action = input("\nWhat action do you want to take? ")
+
+    action = int(action)
 
     # TODO:
     if action == 3:
@@ -180,28 +226,41 @@ def encounter(player, room):
         action = "a"
 
         # Request an action from the player.
-        while not input_validation2(action):
-            action = int(input("\nNext, do you want to attack (1) or flee (2)? "))
+        while not input_validation3(action):
+            action = input("\nNext, do you want to attack (1) or flee (2)? ")
+
+        action = int(action)
 
     # Print the appropriate text for the chosen action and change health and potions if a potions is used.
     if action == 1:
-        print("\n" + room.get_text_attack())
+        attack_action(player, room)
     elif action == 2:
-        print("\n" + room.get_text_flee())
+        flee_action(player, room)
 
-    # Request an <enter> to proceed.
-    input("\nPress <enter> to advance to the next room:")
+
+# Check for a win, loss or draw
+def win_loss_draw(player):
+
+    # Indicate that the player has won the game.
+    if player.get_player_health() > 0 and player.has_potion():
+        print("\nVictory! You escaped the dungeon with your life and the health potion.")
+        input("\nPress <enter> to exit the program and continue your adventures.")
+    # Indicate a loss.
+    elif player.get_player_health() <= 0:
+        print("\nYou died. The game is over.")
+        input("\nPress <enter> to exit the program.")
+    # Indicate a draw.
+    elif player1.get_player_health() > 0 and not player.has_potion():
+        print("\nYou have escaped the dungeon with your life. But you had to leave the health potion behind.")
+        input("\nPress <enter> to exit the program and continue your adventures.")
 
 
 # Add newline characters to the string after every dot to improve readability. Return the new string.
 def add_newline(string):
-
-    new_string = string.replace(". ", ".\n")
-
-    return new_string
+    return string.replace(". ", ".\n")
 
 
-# Add a function to check if a given string is a number between 0 and 4 (inclusive).
+# Check if a given string is a number between 0 and 4 (inclusive).
 def input_validation1(string):
     try:
         int(string)
@@ -213,11 +272,23 @@ def input_validation1(string):
         return False
 
 
-# Add a function to check if a given string is a number between 1 and 3 (inclusive).
+# Check if a given string is a number between 1 and 3 (inclusive).
 def input_validation2(string):
     try:
         int(string)
         if 1 <= int(string) <= 3:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
+
+# Check if a given string is a number between 1 and 2 (inclusive).
+def input_validation3(string):
+    try:
+        int(string)
+        if 1 <= int(string) <= 2:
             return True
         else:
             return False
@@ -239,15 +310,20 @@ def main():
     # Play an encounter for every room in the playlist.
     for i in range(len(room_playlist)):
 
-        # After the 1st round, give the player a health potion.
-        if i == 1:
-            player1.add_potion()
+        # Play the next encounter.
+        encounter(player1, room_playlist[i], i, len(room_playlist))
 
-        # Start the first encounter.
-        encounter(player1, room_playlist[i])
+        # Stop playing if the player's health reaches 0 after a round.
+        if player1.get_player_health() <= 0:
+            break
 
-    # Print a victory message.
-    print("\nYou have escaped the dungeon with your life!")
+        # Ask to proceed to the next room if the game is not finished.
+        if i < len(room_playlist) - 1:
+            # Request an <enter> to proceed to the next room.
+            input("\nPress <enter> to advance to the next room:")
+
+    # Check if the player has won, lost or drew the game.
+    win_loss_draw(player1)
 
 
 main()

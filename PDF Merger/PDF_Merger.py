851ce7d7151b11e91012d2b823e198eb54e57bf0
Danny-Heijnen <78494232+Danny-Heijnen@users.noreply.github.com>
2021-03-31 18:35:11 +0200

    Final Update
    Added correct sorting of the PDF documents and created a new PDF_Merger.exe

--- PDF Merger/PDF_Merger.py
+++ PDF Merger/PDF_Merger.py
@@ -1,11 +1,11 @@
 import os
 from PyPDF2 import PdfFileMerger
 
-print("This program will take multiple PDF files and merge them together into one file.\n\nIn order to do this, create a folder and place the separate PDF files into the folder.\nThen rename the files 1, 2, 3 ...  in the order you want to merge them.\nMake sure this program is in the same folder.")
+print("This program will take multiple PDF files and merge them together into one document.\n\nIn order to do this, create a folder and place the separate PDF files into the folder.\nThen rename the files 1, 2, 3 ...  in the order you want to merge them.\nMake sure this program is in the same folder.")
 
 print("\nIf you have already prepared the folder and filenames, enter 'y' to create the merged PDF.\nIf you have not prepared, please enter 'n' and restart the program when you're done.")
 
-print("\nNote: You will likely get a message saying 'PdfReadWarning', this is not an error, so you can ignore that message.")
+print("\nNote: You will likely get a message saying 'PdfReadWarning'. This is not an error, the message can be ignored.")
 
 answer = ""
 
@@ -17,6 +17,8 @@ if answer.lower() == "n":
 else:
     list_of_pdfs = [input_pdf for input_pdf in os.listdir() if input_pdf.endswith(".pdf") and not input_pdf == "result.pdf"]
 
+    list_of_pdfs.sort()
+
     merger = PdfFileMerger()
 
     for pdf in list_of_pdfs:
@@ -26,4 +28,4 @@ else:
         with open("result.pdf", "wb") as output:
             merger.write(output)
     except:
-        print("\nThe program has encountered an error, perhaps the folder already contains a file called 'result.pdf'\nPlease delete that file or move it to a different folder.\nAfter this step, please restart the program.")
+        print("\nThe program has encountered an error, please check if the file 'result.pdf' contains the desired content.")
